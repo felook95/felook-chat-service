de8f84ae8e5d31a0723b303f37baba376ed4f520
Martin <marta.martin@innoteq.hu>
2020-05-13 20:02:18 +0200

    Initial Commit

---
+++ src/main/java/hu/martin/felookchatservice/FelookChatServiceApplication.java
@@ -0,0 +1,13 @@
+package hu.martin.felookchatservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FelookChatServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FelookChatServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/martin/felookchatservice/FelookChatServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.martin.felookchatservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FelookChatServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
