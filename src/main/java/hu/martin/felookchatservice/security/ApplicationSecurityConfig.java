4d1341ea32303ee0595e4b0cc992d42a90b50bfd
Márta Martin <marta.martin@innoteq.hu>
2020-05-25 15:59:53 +0200

    Spring Security Config

--- src/main/java/hu/martin/felookchatservice/security/ApplicationSecurityConfig.java
+++ src/main/java/hu/martin/felookchatservice/security/ApplicationSecurityConfig.java
@@ -1,9 +1,20 @@
 package hu.martin.felookchatservice.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 @EnableWebFluxSecurity
 public class ApplicationSecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return http.authorizeExchange()
+                .anyExchange().authenticated()
+                .and().build();
+    }
+
 }
