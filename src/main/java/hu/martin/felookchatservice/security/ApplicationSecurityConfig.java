6e5905c9f784a1df3a0f065c8ec9d6a7e2251edc
Martin <marta.martin@innoteq.hu>
2020-06-01 18:39:23 +0200

    Cors for all origin added

--- src/main/java/hu/martin/felookchatservice/security/ApplicationSecurityConfig.java
+++ src/main/java/hu/martin/felookchatservice/security/ApplicationSecurityConfig.java
@@ -13,6 +13,14 @@ import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.authentication.AuthenticationWebFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.server.ServerWebExchange;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -37,10 +45,18 @@ public class ApplicationSecurityConfig {
                 .authorizeExchange()
                 .pathMatchers("/registration").permitAll()
                 .pathMatchers("/login").permitAll()
+                .pathMatchers("/logout").permitAll()
                 .anyExchange().authenticated()
                 .and()
                 .addFilterAt(authenticationWebFilter, SecurityWebFiltersOrder.AUTHENTICATION)
                 .httpBasic().disable()
+                .cors().configurationSource(serverWebExchange -> {
+                    CorsConfiguration corsConfig = new CorsConfiguration();
+                    corsConfig.applyPermitDefaultValues();
+                    corsConfig.setAllowedOrigins(Collections.singletonList(serverWebExchange.getRequest().getHeaders().getOrigin()));
+                    corsConfig.setAllowCredentials(true);
+                    return corsConfig;
+                }).and()
                 .csrf().disable()
                 .formLogin().disable()
                 .logout().disable()
